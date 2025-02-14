ee390cad5f17352b67ab3820366225ed4e26960e
Efrem <Efremdaniel27@gmail.com>
2025-02-14 14:37:46 +0100

    -

--- code_alongs/05_dockerfile/main.py
+++ code_alongs/05_dockerfile/main.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 data_path = Path(__file__).parent/"Data"
 
-print(data_path / 'prog_book.csv')
-
 df = pd.read_csv(data_path/"prog_book.csv")
 print(df.head())
 
+
+df.head().to_csv(data_path/"prog_book_head.csv")
 
 df.head().to_csv(data_path/"prog_book_head.csv")
